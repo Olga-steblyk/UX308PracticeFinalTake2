c24c8c751974468fc4600936e27a7c5579772b9d
Olga-Stebko <78696791+Olga-steblyk@users.noreply.github.com>
2025-04-17 00:51:08 +0000

    entered all 5 questions and tests

--- src/components.jsx
+++ src/components.jsx
@@ -1,23 +1,55 @@
-import {fahrenheitToCelsius, hello} from './functions.js';
+import {colourmix, largest_product, day_of_the_week, pay_raise, leap_year} from './functions.js';
 
 function Question1(){
     return <section>
-1. Write a function that takes a _name_ as an argument and _returns a string_ that _contains "hello"_ and the name paramerer. Test at least 3 names. <br />*Note ... I solved this one already
-      <h2>results</h2>
-      <p>hello("Rich") == "{hello('Rich')}"</p>
-      <p>hello("Bill") == "{hello('Bill')}"</p>
-      <p>hello("Chris") == "{hello('Chris')}"</p>
+1. Write a function that  Determines the secondary rgb_colour from mixing two primary
+RGB (Red, Green, Blue) colours/
+      <h2>Results of function 'colourmix'</h2>
+      <p>colourmix('red', 'blue') == "{colourmix("blue", "red")}"</p>
+      <p>colourmix('green', 'red') == "{colourmix("green", "red")}"</p>
+      <p>colourmix('blue', 'blue') == "{colourmix("blue", "blue")}"</p>
     </section>;
 }
 
 function Question2(){
     return <section>
-2. Write a function that converts from fahrenheit to celsius        
-<h2>results</h2>
-<p>fahrenheitToCelsius(32) == "{fahrenheitToCelsius(32)}"</p>
-<p>fahrenheitToCelsius(212) == "{fahrenheitToCelsius(212)}"</p>
-<p>fahrenheitToCelsius(70) == "{fahrenheitToCelsius(70)}"</p>
+2. Write a function that multiplies the largest 2 numbers from a set of three    
+<h2>Results of function largest product</h2>
+<p>largest_product(7,5,4) == "{largest_product(7,5,4)}"</p>
+<p>largest_product(7,3,4) == "{largest_product(7,3,4)}"</p>
+<p>largest_product(8,9,2) == "{largest_product(8,9,2)}"</p>
     </section>
 }
 
-export {Question1, Question2}
+function Question3(){
+    return <section>
+    3. Write a function that Returns the name of a day of the week given an integer day number.
+    <h2>Results of name of the day</h2>
+    <p>day_of_the_week(2) == "{day_of_the_week(6)}"</p>
+    <p>day_of_the_week(2) == "{day_of_the_week(3)}"</p>
+    <p>day_of_the_week(2) == "{day_of_the_week(2)}"</p>
+        </section>
+}
+
+function Question4(){
+    return <section>
+    4. Calculates pay raises for employees. Pay raises are based on status: Full Time ('F)' or Part Time ('P') and years of service
+    <h2>Results of pay raise function</h2>
+    <p>pay_raise(2) == "{pay_raise("F", 11, 100000)}"</p>
+    <p>pay_raise(2) == "{pay_raise("P", 12, 80000)}"</p>
+    <p>pay_raise(2) == "{pay_raise("F", 2, 40000)}"</p>
+        </section>
+}
+//leap_year
+
+function Question5(){
+    return <section>
+    4. Calculates pay raises for employees. Pay raises are based on status: Full Time ('F)' or Part Time ('P') and years of service
+    <h2>Results of pay raise function</h2>
+    <p>pay_raise(2) == "{leap_year(4598)}"</p>
+    <p>pay_raise(2) == "{leap_year(5678)}"</p>
+    <p>pay_raise(2) == "{leap_year(3200)}"</p>
+        </section>
+}
+
+export {Question1, Question2, Question3, Question4, Question5}
--- src/functions.js
+++ src/functions.js
@@ -7,4 +7,88 @@ function fahrenheitToCelsius(fahrenheit){
     return celsius;
 }
 
-export {hello, fahrenheitToCelsius}
+function colourmix(rgb_colour1,rgb_colour2){
+    let rgb_colour;
+
+    if (rgb_colour1 == "blue" && rgb_colour2 == "red" || rgb_colour1 == "red" && rgb_colour2 == "vlue"){
+        return rgb_colour = "Fuchsia";
+    } else if (rgb_colour1 == "green" && rgb_colour2 == "red" || rgb_colour1 == "red" && rgb_colour2 == "green"){
+        return rgb_colour = "Yellow";
+    } else if (rgb_colour1 == "blue" && rgb_colour2 == "green" || rgb_colour1 == "green" && rgb_colour2 == "blue"){
+        return rgb_colour = "Aqua";
+    } else if (rgb_colour1 && rgb_colour2 == "red"){
+        return rgb_colour = "Red";
+    } else if (rgb_colour1 && rgb_colour2 == "blue"){
+        return rgb_colour = "Blue";
+    } else if (rgb_colour1 && rgb_colour2 == "green"){
+        return rgb_colour = "Green";
+    } else {
+        return rgb_colour = "Your Input was invalid please try again";
+    }
+ 
+}
+
+function largest_product(val1,val2,val3){
+    let product;
+
+    if (val1 > val3 && val2 > val3){
+        return product = val1*val2;
+    }else if (val1 > val2 && val3 > val2){
+        return product = val1*val3;
+    }else if (val3 > val1 && val2 > val1){
+        return product = val3*val3;
+    }else if (val2 == val1 && val1> val3){
+        return product = val1*val2;
+    }else if (val2 == val3 && val3>val1){
+        return product =val2*val3;
+    }
+}
+   
+function day_of_the_week(day_num){
+    let theday;
+    if (day_num == 1){
+        return theday ="It is Sunday";
+    }else if (day_num == 2){
+        return theday ="Its is Monday"
+    }else if (day_num == 3){
+        return theday ="Its is Tuesday"
+    }else if (day_num == 4){
+        return theday ="Its is Wednesday"
+    }else if (day_num == 5){
+        return theday ="Its is Thursday"
+    }else if (day_num == 6){
+        return theday ="Its is Friday"
+    }else if (day_num == 7){
+        return theday ="Its is Saturday"
+    }else{
+        return theday ="Sorry your input is invalid" 
+    }
+}
+
+function pay_raise(status, years, salary){
+    let raise;
+    if(status == "F" && years >10  && salary >0){
+        return raise = salary * 1.05;
+    }else if (status == "F" && years <10 && salary >0){
+        return raise = salary * 1.015;
+    }else if(status == "P" && years >10 && salary >0){
+        return raise = salary * 1.03;
+    }else if (status == "P" && years<4 && years > 0 && salary >0){
+        return raise = salary * 1.01;
+    }else {
+        return raise = salary * 1.02;
+    }
+}
+
+function leap_year(year){
+    var leapyearverdict;
+    if (year % 400 == 0){
+        return leapyearverdict = ` The Year ${year} is in fact a leap year :D`;
+    }else if (year % 100 != 0){
+        return leapyearverdict = ` The Year ${year} is not a leap year D:`;
+    }else{
+        return leapyearverdict = `Sorry your input was invalid, please try again`;
+    }
+}
+
+export {hello, fahrenheitToCelsius, colourmix, largest_product, day_of_the_week, pay_raise, leap_year}
